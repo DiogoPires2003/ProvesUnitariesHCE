a4e4cdc2d9402654113ca52b6e0d44170dee4374
Diogo Filipe Alves Pires <dfalvesp@gmail.com>
2025-12-23 10:54:47 +0100

    Add HealthNationalService interface and mock implementation
    Introduces the HealthNationalService interface and its mock implementation for handling medical history and prescription operations. Adds custom exception classes for error handling.

---
+++ ProvesUnitariesHCE/src/services/HealthNationalService.java
@@ -0,0 +1,14 @@
+package services;
+import services.execeptions.*;
+import medicalConsultation.*;
+
+public interface HealthNationalService {
+    MedicalHistory getMedicalHistory(HealthCardID cip) throws ConnectException, HealthCardIDException;
+
+    MedicalPrescription getMedicalPrescription(HealthCardID cip, String illness) throws ConnectException, HealthCardIDException, AnyCurrentPrescriptionException;
+
+    MedicalPrescription sendHistoryAndPrescription(HealthCardID cip, History hce, String illness, MedicalPrescription mPresc) throws ConnectException, HealthCardIDException, AnyCurrentPrescriptionException, NotCompletedMedicalPrescription;
+
+    // Internal operation
+    MedicalPrescription generateTreatmCodeAndRegister(MedicalPrescription ePresc) throws ConnectException;
+}
